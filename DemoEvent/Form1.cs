8bbda5ffc6676872f67b1abeea751715539dcae4
eabigm <eabigm@IGM2435-25>
2018-11-14 12:00:45 -0500

    Evetns

---
+++ DemoEvent/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoEvent
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text += "!";
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            label1.SetBounds(rand.Next(0, 100), rand.Next(0, 100), rand.Next(20, 100), rand.Next(20, 50));
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.SetBounds(rand.Next(0, 100), rand.Next(0, 100), rand.Next(20, 100), rand.Next(20, 50));
+        }
+    }
+}
